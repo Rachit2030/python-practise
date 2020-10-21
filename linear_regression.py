84437652e15d525b93cb7c2906d6eac120e110cd
Omkarxx <oarora_be19@thapar.edu>
2020-10-21 11:54:32 +0530

    Added the linear regression

---
+++ linear_regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(r'test.csv')
+y = np.array(data.y)
+x = np.array(data.x)
+rows = np.size(x,0)
+features_array = np.empty((rows,2))
+print(features_array.shape)
+for i in range(rows):
+    features_array[i]=(1,x[i])
+print(features_array)
